6795096d577ac4bc4996127ab20b0ca68e4bf5b9
Faris Chebib <faris@theluckybead.com>
2019-01-02 20:09:13 -0700

    Add a README.md

--- learning.py
+++ learning.py
@@ -2,6 +2,6 @@ import requests
 import pprint
 
 pp = pprint.PrettyPrinter(indent=2)
-url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+url = "https://pokeapi.co/api/v2/pokemon/bulbasaur"
 r = requests.get(url)
 pp.pprint(r.json())
